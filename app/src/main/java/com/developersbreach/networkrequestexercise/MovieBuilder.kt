47c915a2e62b6d0aa27b3ab95a6e5c00dc701a1a
pratyu10 <mpratyusha0@gmail.com>
2021-06-15 10:48:24 +0530

    Commit

--- app/src/main/java/com/developersbreach/networkrequestexercise/MovieBuilder.kt
+++ app/src/main/java/com/developersbreach/networkrequestexercise/MovieBuilder.kt
@@ -1,23 +1,26 @@
 package com.developersbreach.networkrequestexercise
 
+import org.json.JSONObject
+import java.net.URL
+
 const val POSTER_APPEND_URL = "https://image.tmdb.org/t/p/w500"
-const val MOVIE_URL = "https://api.themoviedb.org/3/movie/popular?api_key="
+const val MOVIE_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key="
 
 
 fun getMovieData(): List<Movie> {
-
-    // TODO
-
     // First save the string url in variable
+    val stringUrl = MOVIE_URL
 
     // Convert the string to url by passing string to URL object
+    val requestUrl: URL = URL(stringUrl)
 
     // Pass the created url and send a request to https to receive json response
+    val response = getResponseFromHttpsUrl(requestUrl)
 
     // After receiving the json response, send that json data to new function
     // a function which constructs the objects and arrays and gets all properties
-
-    return listOf<Movie>()
+    return fetchJsonMovieData(response)
 }
 
 
@@ -25,24 +28,31 @@ fun fetchJsonMovieData(
     jsonResponse: String
 ): List<Movie> {
 
-    // TODO
-
     // First create a empty ArrayList of type Movie objects.
+    val movieListData = ArrayList<Movie>()
 
     // Create base jsonObject and pass the response
+    val baseJsonObject = JSONObject(jsonResponse)
 
     // Look into nested objects or arrays and create more objects
+    val resultsArray = baseJsonObject.getJSONArray("results")
 
     // If found array, loop inside all objects
+    for (i in 0 until resultsArray.length()) {
+        // Look for more nested objects before observing properties
+        val movieJsonObjects = resultsArray.getJSONObject(i)
 
-    // Look for more nested objects before observing properties
+        // Get all the properties from object lastly created
+        val title = movieJsonObjects.getString("title")
+        val poster = POSTER_APPEND_URL + movieJsonObjects.getString("poster_path")
 
-    // Get all the properties from object lastly created
+        // Return all those properties of type new movie object
+        val movie = Movie(title, poster)
 
-    // Return all those properties of type new movie object
-    // Add each object to empty ArrayList which we have created
+        // Add each object to empty ArrayList which we have created
+        movieListData.add(movie)
+    }
 
     // Finally return the movie list data.
-
-    return listOf<Movie>()
+    return movieListData
 }
--- app/src/main/java/com/developersbreach/networkrequestexercise/NetworkUtils.kt
+++ app/src/main/java/com/developersbreach/networkrequestexercise/NetworkUtils.kt
@@ -1,31 +1,46 @@
 package com.developersbreach.networkrequestexercise
 
+import android.util.Log
 import java.net.URL
+import java.util.*
+import javax.net.ssl.HttpsURLConnection
+import kotlin.Exception
 
 fun getResponseFromHttpsUrl(
     requestUrl: URL?
 ): String {
 
-    // TODO
-
     // Open connection to HttpsURLConnection
+    val urlConnection: HttpsURLConnection = requestUrl?.openConnection() as HttpsURLConnection
+    var response = ""
 
     // Create try catch finally block
+    try {
+        // Inside try block
+        // Return input stream from connection
+        // Create scanner and allow it to stream
+        // Use delimiter pattern and complete the scan
+        // Save the response and close the scanner
 
-    // Inside try block
-    // Return input stream from connection
-    // Create scanner and allow it to stream
-    // Use delimiter pattern and complete the scan
-    // Save the response and close the scanner
+        val inputStream = urlConnection.inputStream
+        val scanner = Scanner(inputStream)
+        scanner.useDelimiter("\\A")
+        if (scanner.hasNext()){
+            response = scanner.next()
+        }
+        scanner.close()
 
-    // Inside catch block
-    // Catch exceptions if necessary and print stack or either log it.
-
-    // Inside finally block
-    // Disconnect if any url connections are open
-
+    } catch (exception: Exception) {
+        // Inside catch block
+        // Catch exceptions if necessary and print stack or either log it.
+        exception.printStackTrace()
+    } finally {
+        // Inside finally block
+        // Disconnect if any url connections are open
+        urlConnection.disconnect()
+    }
     // Return the response
     // Log and observe the json response
-
-    return ""
+    Log.e("NetworkUtils", response)
+    return response
 }
